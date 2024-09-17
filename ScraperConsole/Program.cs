3b6ff7b725fac25f533482a54c0277e6a3cf5efe
Mikkel <10503091@ucn.dk>
2024-09-17 13:30:36 +0200

    yuh

--- ScraperConsole/Program.cs
+++ ScraperConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using TilbudsAvisLibrary;
+﻿using PuppeteerSharp;
+using TilbudsAvisLibrary;
 namespace ScraperConsole
 {
     internal class Program
@@ -7,16 +8,24 @@ namespace ScraperConsole
         {
             string result = await Scraper.FindAvisUrl("https://rema1000.dk/avis");
 
-            Console.ReadLine();
-
             var outResult = await Scraper.CallUrl(result);
 
             //Console.WriteLine(outResult);
 
-            // Save outResult to a txt file
-            File.WriteAllText("response.txt", outResult);
+            File.WriteAllText("response.html", outResult);
 
-            //Console.WriteLine(result);
+            Console.WriteLine(result);
+
+            Console.WriteLine(GetImageUrl(outResult));
+        }
+
+        private static string GetImageUrl(string input)
+        {
+            string searchString = "background-image: url(\"";
+            int startIndex = input.IndexOf(searchString) + searchString.Length;
+            int endIndex = input.IndexOf("\"", startIndex);
+
+            return input.Substring(startIndex, endIndex - startIndex);
         }
     }
 }
--- TilbudsAvisLibrary/Scraper.cs
+++ TilbudsAvisLibrary/Scraper.cs
@@ -14,65 +14,32 @@ namespace TilbudsAvisLibrary
             await new BrowserFetcher().DownloadAsync();
 
             // Randomize user agents
-            var userAgents = new[]
-            {
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
-                // Add more user agents if needed
-            };
             var random = new Random();
-            var randomUserAgent = userAgents[random.Next(userAgents.Length)];
 
             // Launch a headless browser
             using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = true,
-                Args = new[] { "--no-sandbox", "--disable-setuid-sandbox", "--disable-blink-features=AutomationControlled" }
+                Args = new[] { "--disable-web-security", "--no-sandbox", "--disable-setuid-sandbox", "--disable-blink-features=AutomationControlled" }
             });
 
             // Create a new page
             using var page = await browser.NewPageAsync();
 
-            // Set viewport to a random size to mimic different devices
-            await page.SetViewportAsync(new ViewPortOptions
-            {
-                Width = 1280 + random.Next(0, 100),
-                Height = 720 + random.Next(0, 100)
-            });
-
             // Set extra HTTP headers, including randomized user-agent
             await page.SetExtraHttpHeadersAsync(new Dictionary<string, string>
             {
-                { "user-agent", randomUserAgent },
+                { "user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" },
                 { "upgrade-insecure-requests", "1" },
                 { "accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8" },
                 { "accept-encoding", "gzip, deflate, br" },
                 { "accept-language", "en-US,en;q=0.9,en;q=0.8" }
             });
 
-            // Spoof navigator.webdriver property
-            await page.EvaluateFunctionOnNewDocumentAsync("() => { Object.defineProperty(navigator, 'webdriver', { get: () => false }); }");
-
-            // Block resource-heavy requests
-            await page.SetRequestInterceptionAsync(true);
-            page.Request += (sender, e) =>
-            {
-                if (e.Request.ResourceType == ResourceType.Image || e.Request.ResourceType == ResourceType.StyleSheet || e.Request.ResourceType == ResourceType.Font)
-                    e.Request.AbortAsync();
-                else
-                    e.Request.ContinueAsync();
-            };
-
-            // Go to the URL
             await page.GoToAsync(fullUrl);
 
-            // Scroll down to simulate user interaction
-            await page.EvaluateExpressionAsync("window.scrollBy(0, window.innerHeight)");
-
-            // Add a small delay to mimic human behavior
             await Task.Delay(500 + random.Next(500));
 
-            // Get the HTML content of the page
             var content = await page.GetContentAsync();
 
             return content;
@@ -89,8 +56,6 @@ namespace TilbudsAvisLibrary
             string substring = response.Substring(startIndex, endIndex - startIndex);
 
             return url + "/" + substring + "/1";
-
         }
-
     }
 }
