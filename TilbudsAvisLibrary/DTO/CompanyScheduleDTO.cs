b73bc45a308bea2476683bb0327eea0196f76d56
MikkelVibe <10503091@ucn.dk>
2025-02-08 18:44:03 +0100

    Initial setup

---
+++ TilbudsAvisLibrary/DTO/CompanyScheduleDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TilbudsAvisLibrary.DTO
+{
+    public class CompanyScheduleDTO
+    {
+        public int Id { get; set; }           // Company ID matching scraper key
+        public string Name { get; set; }      // Company name
+        public bool IsActive { get; set; }    // Whether to schedule scrapes
+        public DateTime ValidTo { get; set; } // From last successful scrape
+        public DateTime NextExpectedRelease { get; set; } // Calculated (ValidTo + buffer)
+        public int RetryCount { get; set; }   // Current retry attempt (0 = success)
+        public string LastError { get; set; } // Last error message
+    }
+}
