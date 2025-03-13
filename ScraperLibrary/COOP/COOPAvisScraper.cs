1e521f8cf2e012a3c6258d91b9bdf1efb02b49b7
Mikkel <10503091@ucn.dk>
2025-03-13 08:39:53 +0100

    Stop bot being stopped

--- ScraperLibrary/COOP/COOPAvisScraper.cs
+++ ScraperLibrary/COOP/COOPAvisScraper.cs
@@ -63,7 +63,12 @@ namespace ScraperLibrary.COOP
                         tcs.TrySetResult(request.Url.Substring(baseUrl.Length));
                     }
                 };
+
+                await page.SetUserAgentAsync("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+
+
                 await page.GoToAsync(url);
+
 
                 var timeoutTask = Task.Delay(timeoutMs);
 
